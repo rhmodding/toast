045d27ff5d5eef6e099c57b8417ca1f5db4cbe2b
yeetmydog <48489278+yeetmydog@users.noreply.github.com>
2024-05-05 18:46:41 +0200

    Implement console splash

---
+++ src/ConsoleSplash.hpp
@@ -0,0 +1,21 @@
+#ifndef CONSOLESPLASH_HPP
+#define CONSOLESLASH_HPP
+
+const char* consoleSplash =
+"                                                 " "\n"
+"  ,d                                      ,d     " "\n"
+"  88                                      88     " "\n"
+"MM88MMM ,adPPYba,  ,adPPYYba, ,adPPYba, MM88MMM  " "\n"
+"  88   a8"     "8a \"\"     `Y8 I8[    ""   88     " "\n"
+"  88   8b       d8 ,adPPPPP88  `\"Y8ba,    88     " "\n"
+"  88,  \"8a,   ,a8\" 88,    ,88 aa    ]8I   88,    " "\n" 
+"  \"Y888 `\"YbbdP\"'  `\"8bbdP\"Y8 `\"YbbdP\"'   \"Y888  " "\n"
+"                                                 " "\n"
+"                                                 " "\n"
+"        the ultimate cellanim modding tool       " "\n"
+"            for Rhythm Heaven Fever              " "\n"
+"                                                 " "\n"
+"         https://github.com/conhlee/toast        "
+;
+
+#endif // CONSOLESPLASH_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,16 @@
 
 #include "AppState.hpp"
 
+#include "ConsoleSplash.hpp"
+
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
 // Main code
 int main(int argc, char** argv) {
+    std::cout << consoleSplash << "\n\n";
+
     new App;
 
     extern App* gAppPtr;
