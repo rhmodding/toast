58521e012afbb6f6269cc7f378d49ed1ee01c236
yeetmydog <48489278+yeetmydog@users.noreply.github.com>
2024-05-08 00:19:10 +0200

    Add image editor path to config window

--- src/window/WindowConfig.cpp
+++ src/window/WindowConfig.cpp
@@ -22,6 +22,40 @@ const char* categoryNames[] = {
     "Paths"
 };
 
+bool CPPSTRING_InputText(const char* label, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = nullptr, void* user_data = nullptr) {
+    struct InputTextCallback_UserData {
+        std::string*            Str;
+        ImGuiInputTextCallback  ChainCallback;
+        void*                   ChainCallbackUserData;
+    };
+    
+    flags |= ImGuiInputTextFlags_CallbackResize;
+
+    InputTextCallback_UserData cb_user_data;
+    cb_user_data.Str = str;
+    cb_user_data.ChainCallback = callback;
+    cb_user_data.ChainCallbackUserData = user_data;
+    return ImGui::InputText(label, (char*)str->c_str(), str->capacity() + 1, flags, [](ImGuiInputTextCallbackData* data) -> int {
+        InputTextCallback_UserData* user_data = (InputTextCallback_UserData*)data->UserData;
+        if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
+        {
+            // Resize string callback
+            // If for some reason we refuse the new length (BufTextLen) and/or capacity (BufSize) we need to set them back to what we want.
+            std::string* str = user_data->Str;
+            IM_ASSERT(data->Buf == str->c_str());
+            str->resize(data->BufTextLen);
+            data->Buf = (char*)str->c_str();
+        }
+        else if (user_data->ChainCallback)
+        {
+            // Forward to user callback, if any
+            data->UserData = user_data->ChainCallbackUserData;
+            return user_data->ChainCallback(data);
+        }
+        return 0;
+    }, &cb_user_data);
+}
+
 void WindowConfig::Update() {
     ImGui::SetNextWindowSize(ImVec2(500, 440), ImGuiCond_FirstUseEver);
     if (ImGui::Begin("Config", &this->open, ImGuiWindowFlags_MenuBar)) {
@@ -79,6 +113,10 @@ void WindowConfig::Update() {
                     const char* themeOptions[] = { "Dark", "Light" };
                     if (ImGui::Combo("Theme", &currentTheme, themeOptions, 2))
                         this->selfConfig.darkTheme = currentTheme == 0;
+                } break;
+
+                case Category_Paths: {
+                    CPPSTRING_InputText("Image Editor Path", &this->selfConfig.imageEditorPath);
                 } break;
                 
                 default:
