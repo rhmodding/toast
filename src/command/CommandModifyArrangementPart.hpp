b5e3fe8a33c48789c974151a5baa14b5ed89861c
yeetmydog <48489278+yeetmydog@users.noreply.github.com>
2024-06-11 22:53:11 +0200

    ModifyArrangementPart command class

---
+++ src/command/CommandModifyArrangementPart.hpp
@@ -0,0 +1,63 @@
+#ifndef COMMANDMODIFYARRANGEMENTPART_HPP
+#define COMMANDMODIFYARRANGEMENTPART_HPP
+
+#include "BaseCommand.hpp"
+
+#include "../anim/RvlCellAnim.hpp"
+
+#include "../AppState.hpp"
+#include "../SessionManager.hpp"
+
+class CommandModifyArrangementPart : public BaseCommand {
+public:
+    // Constructor: Modify part by cellanimIndex, arrangementIndex and finally partIndex.
+    CommandModifyArrangementPart(
+        uint32_t cellanimIndex, uint32_t arrangementIndex, uint32_t partIndex,
+        RvlCellAnim::ArrangementPart newPart
+    ) :
+        cellanimIndex(cellanimIndex), arrangementIndex(arrangementIndex), partIndex(partIndex),
+        newPart(newPart)
+    {
+        this->oldPart = this->getPart();
+    };
+
+    // Constructor: Modify selected part.
+    CommandModifyArrangementPart(
+        RvlCellAnim::ArrangementPart newPart
+    ) :
+        cellanimIndex(cellanimIndex), arrangementIndex(arrangementIndex), partIndex(partIndex),
+        newPart(newPart)
+    {
+        this->cellanimIndex = SessionManager::getInstance().getCurrentSession()->cellIndex;
+        this->arrangementIndex = AppState::getInstance().globalAnimatable->getCurrentKey()->arrangementIndex;
+        this->partIndex = AppState::getInstance().selectedPart;
+
+        this->oldPart = this->getPart();
+    };
+
+    void Execute() override {
+        this->getPart() = this->newPart;
+    };
+
+    void Rollback() override {
+        this->getPart() = this->oldPart;
+    };
+
+private:
+    uint32_t cellanimIndex;
+    uint32_t arrangementIndex;
+    uint32_t partIndex;
+
+    RvlCellAnim::ArrangementPart oldPart;
+    RvlCellAnim::ArrangementPart newPart;
+
+    RvlCellAnim::ArrangementPart& getPart() {
+        return
+            SessionManager::getInstance().getCurrentSession()
+            ->cellanims.at(this->cellanimIndex)
+            ->arrangements.at(this->arrangementIndex)
+            .parts.at(this->partIndex);
+    }
+};
+
+#endif // COMMANDMODIFYARRANGEMENTPART_HPP
