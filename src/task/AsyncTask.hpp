d530269c70705fdf98c569d7da576b916c866a71
yeetmydog <48489278+yeetmydog@users.noreply.github.com>
2024-06-23 18:18:03 +0200

    AsyncTasks

---
+++ src/task/AsyncTask.hpp
@@ -0,0 +1,86 @@
+#ifndef ASYNCTASK_HPP
+#define ASYNCTASK_HPP
+
+#include <thread>
+#include <atomic>
+
+#include "imgui.h"
+#include "imgui_internal.h"
+
+#include <sstream>
+
+class AsyncTask {
+public:
+    AsyncTask(uint32_t id, const char* message) :
+        id(id), isComplete(false),
+        message(message)
+    {
+        char str[32];
+        sprintf(str, "Task%u", id);
+
+        this->imguiID = ImHashStr(str);
+    }
+
+    virtual ~AsyncTask() = default;
+
+    void Start() {
+        this->isComplete = false;
+
+        std::thread([this]() {
+            Run();
+            this->isComplete = true;
+        }).detach();
+    }
+
+    void RenderPopup() {
+        ImGui::PushID(this->imguiID);
+
+        if (!isComplete)
+            ImGui::OpenPopup("Working..");
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 25, 20 });
+        
+        ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(.5f, .5f));
+        if (ImGui::BeginPopupModal("Working..", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+            ImGui::Text(this->message);
+
+            ImGui::Dummy(ImVec2(0.f, 1.f));
+
+            ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImGui::GetStyleColorVec4(ImGuiCol_ButtonHovered));
+            ImGui::ProgressBar(-1.0f * (float)ImGui::GetTime(), ImVec2(.0f, 15.f));
+            ImGui::PopStyleColor();
+
+            ImGui::EndPopup();
+        }
+
+        ImGui::PopStyleVar();
+
+        ImGui::PopID();
+    }
+
+    void TryRunEffect() {
+        if (isComplete && !hasEffectRun) {
+            Effect();
+            hasEffectRun = true;
+        }
+    }
+
+    bool IsComplete() const { return isComplete; }
+    bool HasEffectRun() const { return hasEffectRun; }
+
+protected:
+    virtual void Run() = 0;
+    virtual void Effect() = 0;
+
+private:
+    uint32_t id;
+
+    std::atomic<bool> isComplete;
+    std::atomic<bool> hasEffectRun;
+
+    const char* message;
+
+    ImGuiID imguiID;
+};
+
+#endif // ASYNCTASK_HPP
---
+++ src/task/AsyncTaskManager.hpp
@@ -0,0 +1,68 @@
+#ifndef ASYNCTASKMANAGER_HPP
+#define ASYNCTASKMANAGER_HPP
+
+#include "../Singleton.hpp"
+
+#include "AsyncTask.hpp"
+
+#include <vector>
+
+#include <memory>
+#include <atomic>
+
+// Stores instance of AsyncTaskManager in local asyncTaskManager.
+#define GET_ASYNC_TASK_MANAGER AsyncTaskManager& asyncTaskManager = AsyncTaskManager::getInstance()
+
+class AsyncTaskManager : public Singleton<AsyncTaskManager> {
+    friend class Singleton<AsyncTaskManager>; // Allow access to base class constructor
+
+public:
+    template <typename TaskType, typename... Args>
+    void StartTask(Args&&... args) {
+        auto task = std::make_unique<TaskType>(this->GenerateId(), std::forward<Args>(args)...);
+        task->Start();
+
+        tasks.emplace_back(std::move(task));
+    }
+
+    void UpdateTasks() {
+        for (auto& task : this->tasks) {
+            task->RenderPopup();
+            task->TryRunEffect();
+        }
+
+        this->tasks.erase(
+            std::remove_if(this->tasks.begin(), this->tasks.end(),
+            [](const std::unique_ptr<AsyncTask>& task) {
+                return task->IsComplete() && task->HasEffectRun();
+            }),
+
+            this->tasks.end()
+        );
+    }
+
+    template <typename TaskType>
+    bool HasTaskOfType() const {
+        for (const auto& task : this->tasks) {
+            if (dynamic_cast<TaskType*>(task.get()) != nullptr)
+                return true;
+        }
+
+        return false;
+    }
+
+private:
+    uint32_t GenerateId() {
+        return this->nextId++;
+    }
+
+    std::atomic<uint32_t> nextId;
+    std::vector<std::unique_ptr<AsyncTask>> tasks;
+
+private:
+    AsyncTaskManager() :
+        nextId(0)
+    {}
+};
+
+#endif // ASYNCTASKMANAGER_HPP
