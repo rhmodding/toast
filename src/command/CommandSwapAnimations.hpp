f38a5711d97e15104c2fdc905a16e6f6a2f09fb7
yeetmydog <48489278+yeetmydog@users.noreply.github.com>
2024-07-06 16:59:56 +0200

    Implement animation index swapping

---
+++ src/command/CommandSwapAnimations.hpp
@@ -0,0 +1,99 @@
+#ifndef COMMANDSWAPANIMATIONS_HPP
+#define COMMANDSWAPANIMATIONS_HPP
+
+#include "BaseCommand.hpp"
+
+#include "../anim/RvlCellAnim.hpp"
+
+#include "../AppState.hpp"
+#include "../SessionManager.hpp"
+
+class CommandSwapAnimations : public BaseCommand {
+public:
+    // Constructor: Swap animations by cellanimIndex, animationIndexA and animationIndexB.
+    CommandSwapAnimations(
+        uint16_t cellanimIndex,
+        uint16_t animationIndexA, uint16_t animationIndexB,
+        bool swapNames
+    ) :
+        cellanimIndex(cellanimIndex),
+        animationIndexA(animationIndexA), animationIndexB(animationIndexB),
+        swapNames(swapNames)
+    {}
+
+    void Execute() override {
+        std::swap(
+            this->getAnimations().at(animationIndexA),
+            this->getAnimations().at(animationIndexB)
+        );
+
+        if (this->swapNames) {
+            auto& map = this->getAnimationNames();
+
+            if (map.find(animationIndexA) != map.end() && map.find(animationIndexB) != map.end()) {
+                std::swap(map[animationIndexA], map[animationIndexB]);
+            } else if (map.find(animationIndexA) != map.end()) {
+                map[animationIndexB] = map[animationIndexA];
+                map.erase(animationIndexA);
+            } else if (map.find(animationIndexB) != map.end()) {
+                map[animationIndexA] = map[animationIndexB];
+                map.erase(animationIndexB);
+            }
+        }
+
+        this->updateAnimationState();
+    }
+
+    void Rollback() override {
+        this->Execute();
+    }
+
+private:
+    uint16_t cellanimIndex;
+    uint16_t animationIndexA;
+    uint16_t animationIndexB;
+    
+    bool swapNames;
+
+    void updateAnimationState() {
+        GET_APP_STATE;
+
+        if (
+            appState.selectedAnimation == animationIndexA ||
+            appState.selectedAnimation == animationIndexB
+        ) {
+            appState.selectedAnimation =
+                appState.selectedAnimation == animationIndexA ? animationIndexB :
+                appState.selectedAnimation == animationIndexB ? animationIndexA :
+                appState.selectedAnimation;
+
+            appState.globalAnimatable->setAnimationFromIndex(appState.selectedAnimation);
+
+            appState.playerState.currentFrame = 0;
+            appState.playerState.updateSetFrameCount();
+            appState.playerState.updateCurrentFrame();
+
+            RvlCellAnim::Arrangement* arrangementPtr =
+                appState.globalAnimatable->getCurrentArrangement();
+
+            if (appState.selectedPart >= arrangementPtr->parts.size())
+                appState.selectedPart = static_cast<int16_t>(arrangementPtr->parts.size() - 1);
+        }
+    }
+
+    std::vector<RvlCellAnim::Animation>& getAnimations() {
+        return
+            SessionManager::getInstance().getCurrentSession()
+            ->cellanims.at(this->cellanimIndex).object
+            ->animations;
+    }
+
+    std::unordered_map<uint16_t, std::string>& getAnimationNames() {
+        return
+            SessionManager::getInstance().getCurrentSession()
+            ->cellanims.at(this->cellanimIndex).
+            animNames;
+    }
+};
+
+#endif // COMMANDSWAPANIMATIONS_HPP
--- src/window/WindowInspector/WindowInspector_Animation.cpp
+++ src/window/WindowInspector/WindowInspector_Animation.cpp
@@ -7,6 +7,8 @@
 #include "../../AppState.hpp"
 #include "../../anim/Animatable.hpp"
 
+#include "../../command/CommandSwapAnimations.hpp"
+
 void WindowInspector::Level_Animation() {
     GET_APP_STATE;
     GET_ANIMATABLE;
@@ -43,7 +45,9 @@ void WindowInspector::Level_Animation() {
     RvlCellAnim::Animation* animation = globalAnimatable->getCurrentAnimation();
     static char newMacroName[256]{ "" };
 
-    ImGui::SeparatorText((char*)ICON_FA_PENCIL " Macro name");
+    static int32_t swapAnim{ -1 };
+
+    ImGui::SeparatorText((char*)ICON_FA_PENCIL " Properties");
     if (ImGui::Button("Edit macro name..")) {
         std::string copyName(animationName ? animationName : "");
         if (copyName.size() > 255)
@@ -52,6 +56,17 @@ void WindowInspector::Level_Animation() {
         strcpy(newMacroName, copyName.c_str());
         ImGui::OpenPopup("###EditMacroNamePopup");
     }
+    ImGui::SameLine();
+    if (ImGui::Button("Swap index..")) {
+        swapAnim = -1;
+        ImGui::OpenPopup("###SwapIndexPopup");
+    }
+
+    ImGui::Indent();
+        ImGui::Bullet();
+        ImGui::SameLine();
+        ImGui::TextWrapped("Macro names are not used for mapping animations directly in the game.\nTo swap animations, use [Swap index..].");
+    ImGui::Unindent();
 
     ImVec2 center = ImGui::GetMainViewport()->GetCenter();
     ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
@@ -91,4 +106,101 @@ void WindowInspector::Level_Animation() {
         ImGui::EndPopup();
     }
     ImGui::PopStyleVar();
+
+    ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+    if (ImGui::BeginPopupModal("Swap animation index###SwapIndexPopup", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+        static float animationBegin{ static_cast<float>(ImGui::GetTime()) };
+        const static float animationTime{ .25f };
+
+        static bool swapNames{ true };
+
+        // Left
+        {
+            ImGui::BeginGroup();
+            if (ImGui::BeginChild("Properties", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), ImGuiChildFlags_Border | ImGuiChildFlags_ResizeX)) {
+                if (swapAnim == -1) {
+                    ImGui::Bullet();
+                    ImGui::SameLine();
+                    ImGui::TextWrapped("Select a animation to swap with.");
+                }
+                else {
+                    ImGui::Checkbox("Swap macro names", &swapNames);
+                }
+            }
+            ImGui::EndChild();
+
+            if (ImGui::Button("Cancel"))
+                ImGui::CloseCurrentPopup();
+            ImGui::SameLine();
+
+            ImGui::BeginDisabled(swapAnim == -1);
+            if (ImGui::Button("Apply")) {
+                sessionManager.getCurrentSession()->executeCommand(std::make_shared<CommandSwapAnimations>(
+                CommandSwapAnimations(
+                    sessionManager.getCurrentSession()->currentCellanim,
+                    animationIndex,
+                    swapAnim,
+                    swapNames
+                )));
+
+                changed = true;
+
+                ImGui::CloseCurrentPopup();
+            }
+            ImGui::EndDisabled();
+
+            ImGui::EndGroup();
+        }
+
+        ImGui::SameLine();
+
+        // Right
+        {
+            ImGui::SetNextWindowSizeConstraints({ 400.f, 380.f }, { FLT_MAX, FLT_MAX });
+            ImGui::BeginChild("Visualization", { 0, 0 }, ImGuiChildFlags_Border);
+
+            float beginCursorY = ImGui::GetCursorPosY();
+
+            for (uint16_t n = 0; n < globalAnimatable->cellanim->animations.size(); n++) {
+                std::stringstream fmtStream;
+
+                GET_SESSION_MANAGER;
+
+                uint16_t nI = swapAnim != -1 ? n == animationIndex ? swapAnim : n == swapAnim ? animationIndex : n : n;
+
+                auto query = sessionManager.getCurrentSession()->getAnimationNames().find(swapNames ? n : nI);
+
+                const char* animName =
+                    query != sessionManager.getCurrentSession()->getAnimationNames().end() ?
+                        query->second.c_str() :
+                        "(no macro defined)";
+
+                fmtStream << std::to_string(nI+1) << ". " << animName;
+                if (nI != n)
+                    fmtStream << " (old index: " << n+1 << ")";
+
+                float positionOrig = beginCursorY + (n * (
+                    ImGui::CalcTextSize(fmtStream.str().c_str(), nullptr, true).y +
+                    (ImGui::GetStyle().ItemSpacing.y)
+                ));
+                float positionNew = beginCursorY + (nI * (
+                    ImGui::CalcTextSize(fmtStream.str().c_str(), nullptr, true).y +
+                    (ImGui::GetStyle().ItemSpacing.y)
+                ));
+
+                float animProgression = std::clamp<float>((ImGui::GetTime() - animationBegin) / animationTime, 0.f, 1.f);
+                ImGui::SetCursorPosY(std::lerp(positionOrig, positionNew, Common::EaseInOut(animProgression)));
+
+                if (ImGui::Selectable(fmtStream.str().c_str(), animationIndex == n || swapAnim == n, ImGuiSelectableFlags_DontClosePopups)) {
+                    swapAnim = n;
+
+                    animationBegin = ImGui::GetTime();
+                }
+            }
+            
+            ImGui::EndChild();
+        }
+
+        ImGui::EndPopup();
+    }
 }
