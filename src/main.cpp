ca14e3e5832fb9cc9faf30781961fd8acbc6189a
Angelo <48489278+conhlee@users.noreply.github.com>
2024-10-13 00:07:54 +0200

    main: remove unreachable macro

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,10 @@
 
 #include "ConsoleSplash.hpp"
 
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
-
 int main(int argc, char** argv) {
     std::cout << consoleSplash << "\n\n";
 
-    {
-        App app;
-        while (LIKELY(app.isRunning()))
-            app.Update();
-    }
+    App app;
+    while (LIKELY(app.isRunning()))
+        app.Update();
 }
