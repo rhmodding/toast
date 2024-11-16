47d354bf2a477984f684ba6c6bc4f1c88636e6a8
Angelo <48489278+conhlee@users.noreply.github.com>
2024-11-16 22:04:07 +0100

    Popup_SheetRepackFailed

---
+++ src/App/popups/Popup_SheetRepackFailed.hpp
@@ -0,0 +1,29 @@
+#ifndef POPUP_SHEETREPACKFAILED
+#define POPUP_SHEETREPACKFAILED
+
+#include <imgui.h>
+
+#include "../../common.hpp"
+
+void Popup_SheetRepackFailed() {
+    CENTER_NEXT_WINDOW;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 25.f, 20.f });
+    if (ImGui::BeginPopupModal("The spritesheet could not be repacked.###SheetRepackFailed", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+        ImGui::TextUnformatted(
+            "The spritesheet could not not be repacked;\n"
+            "no changes have been made."
+        );
+
+        ImGui::Dummy({ 0.f, 5.f });
+
+        if (ImGui::Button("Alright", { 120.f, 0.f }))
+            ImGui::CloseCurrentPopup();
+        ImGui::SetItemDefaultFocus();
+
+        ImGui::EndPopup();
+    }
+    ImGui::PopStyleVar();
+}
+
+#endif // POPUP_TEXTUREEXPORTFAILED
