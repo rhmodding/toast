dd138b6782493c4c8d8e035c8b6f1538798e7488
Angelo <48489278+conhlee@users.noreply.github.com>
2025-03-10 02:04:34 +0100

    glInclude: restructure

--- src/glInclude.hpp
+++ src/glInclude.hpp
@@ -1,12 +1,11 @@
-#if defined(__WIN32__) || defined(__linux__)
-#define USING_GLEW
-#include <GL/glew.h>
-#endif // defined(__WIN32__) || defined(__linux__)
-
 #if defined(__APPLE__)
 #include <OpenGL/gl.h>
 #include <OpenGL/glext.h>
 #else
+// GLEW is needed on Windows & Linux; macOS doesn't need it.
+#define USING_GLEW
+#include <GL/glew.h>
+
 #include <GL/gl.h>
 #include <GL/glext.h>
 #endif // defined(__APPLE__)
