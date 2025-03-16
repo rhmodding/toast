dc3bc55c0f0f6ff347142da4784b57c0d7518dfb
Angelo <48489278+conhlee@users.noreply.github.com>
2025-03-16 03:44:01 +0100

    main: set locale to C.UTF-8

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,13 @@
 
 #include "ConsoleSplash.hpp"
 
+#include <iostream>
+
+#include <locale>
+
 int main(int argc, const char** argv) {
+    std::setlocale(LC_ALL, "C.UTF-8");
+
     std::cout << consoleSplash << "\n" << std::endl;
 
     Toast toast (argc, argv);
