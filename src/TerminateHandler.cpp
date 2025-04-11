9cac91c97de969b25849cdcff2189232ef40ad7e
Angelo <48489278+conhlee@users.noreply.github.com>
2025-04-11 23:07:51 +0200

    Split TerminateHandler out of Toast

---
+++ src/TerminateHandler.cpp
@@ -0,0 +1,46 @@
+#include "TerminateHandler.hpp"
+
+#include <exception>
+
+#include "CxxDemangle.hpp"
+
+#include "Logging.hpp"
+
+#include <tinyfiledialogs.h>
+
+static void onTerminate() {
+    try {
+        std::exception_ptr exceptionPointer { std::current_exception() };
+        if (exceptionPointer)
+            std::rethrow_exception(exceptionPointer);
+        else
+            Logging::err << "The terminate handler was invoked with no exception." << std::endl;
+    }
+    catch (const std::exception& ex) {
+        Logging::err <<
+            "An exception of type " << CxxDemangle::Demangle(typeid(ex).name()) << " was caught: " << ex.what() << std::endl;
+    }
+    catch (...) {
+        Logging::err << "An unknown exception was thrown." << std::endl;
+    }
+
+    Logging::err << "This is a fatal error; toast will now be closed." << std::endl;
+
+    tinyfd_messageBox( \
+        "toast has crashed", \
+        "toast has encountered a fatal error and cannot continue execution.\n" \
+        "You can check the log for more details (toast.log). toast will now close.", \
+        "ok", "error", 1 \
+    );
+
+    Logging::Close();
+
+    std::exit(1);
+}
+
+void TerminateHandler::Init() {
+    // In debug builds we want the exception to stay unhandled, otherwise the debugger won't catch it.
+#ifndef NDEBUG
+    std::set_terminate(onTerminate);
+#endif
+}
---
+++ src/TerminateHandler.hpp
@@ -0,0 +1,10 @@
+#ifndef TERMINATEHANDLER_HPP
+#define TERMINATEHANDLER_HPP
+
+namespace TerminateHandler {
+
+void Init();
+
+} // namespace TerminateHandler
+
+#endif // TERMINATEHANDLER_HPP
