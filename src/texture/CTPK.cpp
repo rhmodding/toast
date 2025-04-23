77c81e7b0f567dade3e0001514ddc3dbcbae4c8e
Angelo <48489278+conhlee@users.noreply.github.com>
2025-04-23 19:09:11 +0200

    CTPKTexture: always set GPU texture wrap mode to clamp

--- src/texture/CTPK.cpp
+++ src/texture/CTPK.cpp
@@ -134,8 +134,9 @@ GLuint CTPKTexture::createGPUTexture() const {
         glGenTextures(1, &textureId);
         glBindTexture(GL_TEXTURE_2D, textureId);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        // CTPK textures are always clamped.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
