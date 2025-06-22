8bf67679ef94e94712d831ef54b57d3738770e65
Angelo <48489278+conhlee@users.noreply.github.com>
2025-06-22 22:41:45 +0200

    WindowInspector: refactor & improve anim lvl

--- src/window/WindowInspector/LvlAnimation.cpp
+++ src/window/WindowInspector/LvlAnimation.cpp
@@ -4,13 +4,44 @@
 #include "manager/ThemeManager.hpp"
 #include "manager/PlayerManager.hpp"
 
-#include "command/CommandSetAnimationInterpolated.hpp"
+#include "manager/AppState.hpp"
 
-#include "font/FontAwesome.h"
+#include "command/CommandModifyAnimation.hpp"
 
 #include "App/Popups.hpp"
 
-#include "manager/AppState.hpp"
+#include "util/UIUtil.hpp"
+
+#include "font/FontAwesome.h"
+
+static std::string sanitizeAnimationName(std::string newName, bool isCtr) {
+    if (newName.empty())
+        return newName;
+    
+    if (!isCtr) {
+        // Prefix an underscore if the first character is a digit.
+        if (std::isdigit(newName[0])) {
+            newName.insert(newName.begin(), '_');
+        }
+
+        // Replace invalid characters with underscores.
+        for (char& c : newName) {
+            if (!std::isalnum(static_cast<unsigned char>(c)) && c != '_') {
+                c = '_';
+            }
+        }
+    }
+    // On CTR we simply replace non-alnum and non-underscore characters.
+    else {
+        for (char& c : newName) {
+            if (!std::isalnum(static_cast<unsigned char>(c)) && c != '_') {
+                c = '_';
+            }
+        }
+    }
+
+    return newName;
+}
 
 void WindowInspector::Level_Animation() {
     PlayerManager& playerManager = PlayerManager::getInstance();
@@ -22,63 +53,96 @@ void WindowInspector::Level_Animation() {
 
     ImGui::SameLine();
 
+    CellAnim::Animation& animation = playerManager.getAnimation();
     unsigned animationIndex = playerManager.getAnimationIndex();
-    const char* animationName = playerManager.getAnimation().name.c_str();
-    if (animationName[0] == '\0')
-        animationName = "(no name set)";
+
+    CellAnim::Animation newAnimation = playerManager.getAnimation();
+    CellAnim::Animation originalAnimation = playerManager.getAnimation();
 
     ImGui::BeginChild("LevelHeader", { 0.f, 0.f }, ImGuiChildFlags_AutoResizeY);
-    {
+
         ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, { 0.f, 0.f });
 
         ImGui::Text("Animation no. %u:", animationIndex + 1);
 
         ImGui::PushFont(ThemeManager::getInstance().getFonts().large);
-        ImGui::TextWrapped("%s", animationName);
+        ImGui::TextWrapped("%s",
+            animation.name.empty() ? "(no name set)" : animation.name.c_str()
+        );
         ImGui::PopFont();
 
         ImGui::PopStyleVar();
-    }
-    ImGui::EndChild();
+
+        ImGui::EndChild();
 
     ImGui::SeparatorText((const char*)ICON_FA_PENCIL " Properties");
 
     if (isCtr) {
-        bool isInterpolated = playerManager.getAnimation().isInterpolated;
-
-        if (ImGui::Checkbox("Interpolated", &isInterpolated)) {
-            const auto& currentSession = sessionManager.getCurrentSession();
-
-            currentSession->addCommand(
-            std::make_shared<CommandSetAnimationInterpolated>(
-                currentSession->getCurrentCellAnimIndex(),
-                animationIndex,
-                isInterpolated
-            ));
-        }
+        ImGui::Checkbox("Interpolated", &newAnimation.isInterpolated);
 
         ImGui::Dummy({ 0.f, 5.f });
     }
 
-    if (ImGui::Button("Edit animation name..")) {
-        Popups::_editAnimationNameIdx = animationIndex;
-        OPEN_GLOBAL_POPUP("###EditAnimationName");
+    UIUtil::Widget::ValueEditor<std::string>("Name", animation.name,
+        [&]() { return originalAnimation.name; },
+        [&](const std::string& oldValue, const std::string& newValue) {
+            originalAnimation.name = oldValue;
+            newAnimation.name = sanitizeAnimationName(newValue, isCtr);
+        },
+        [](const char* label, std::string* value) {
+            return UIUtil::Widget::StdStringTextInput(label, *value);
+        }
+    );
+
+    if (!isCtr) {
+        UIUtil::Widget::ValueEditor<std::string>("Comment", animation.comment,
+            [&]() { return originalAnimation.comment; },
+            [&](const std::string& oldValue, const std::string& newValue) {
+                originalAnimation.comment = oldValue;
+                newAnimation.comment = newValue;
+            },
+            [](const char* label, std::string* value) {
+                return UIUtil::Widget::StdStringTextInput(label, *value);
+            }
+        );
     }
 
-    ImGui::SameLine();
+    ImGui::Dummy({ 0.f, 2.f });
+
+    ImGui::Separator();
+
+    ImGui::Dummy({ 0.f, 1.f });
+
     if (ImGui::Button("Swap animation..")) {
         Popups::_swapAnimationIdx = animationIndex;
         OPEN_GLOBAL_POPUP("###SwapAnimation");
     }
 
     if (!isCtr) {
+        ImGui::Dummy({ 0.f, 1.f });
+
+        ImGui::Separator();
+
+        ImGui::Dummy({ 0.f, 2.f });
+
         ImGui::Indent();
             ImGui::Bullet(); ImGui::SameLine();
             ImGui::TextWrapped(
-                "Animation names are not used for mapping animations directly in the game; instead\n"
-                "the animation index is used (the ordering of the animation).\n"
-                "To swap animations, select [ Swap animation.. ]."
+                "Animation names are not used to map animations in-game; instead, "
+                "the animation index (its order in the list) determines mapping.\n"
+                "To swap animations, use [Swap animation..]."
             );
         ImGui::Unindent();
+    }
+
+    if (newAnimation != originalAnimation) {
+        animation = originalAnimation;
+
+        sessionManager.getCurrentSession()->addCommand(
+        std::make_shared<CommandModifyAnimation>(
+            sessionManager.getCurrentSession()->getCurrentCellAnimIndex(),
+            playerManager.getAnimationIndex(),
+            newAnimation
+        ));
     }
 }
