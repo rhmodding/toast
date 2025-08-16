1a46ed5a186214869e25126e5fee1a6f69da922d
0xadk <0xadk@users.noreply.github.com>
2025-08-15 23:40:57 -0700

    Logging: add duplicate log filter

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -2,18 +2,29 @@
 
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/dup_filter_sink.h>
 #include <spdlog/spdlog.h>
+
+namespace sink = spdlog::sinks;
+
+template<typename T, typename... Args>
+static std::shared_ptr<spdlog::logger> makeDedupedLogger(std::string name, Args &&...args) {
+    auto dedup_sink = std::make_shared<sink::dup_filter_sink_mt>(std::chrono::seconds(10));
+    dedup_sink->add_sink(std::make_shared<T>(std::forward<Args>(args)...));
+    dedup_sink->set_pattern("[%Y-%m-%d %H:%M:%S] [%^%L%$] %v");
+    return std::make_shared<spdlog::logger>(std::move(name), dedup_sink);
+}
 
 std::shared_ptr<spdlog::logger> Logging::sStdoutLogger = nullptr;
 std::shared_ptr<spdlog::logger> Logging::sStderrLogger = nullptr;
 std::shared_ptr<spdlog::logger> Logging::sFileLogger = nullptr;
 
 void Logging::Open(std::string_view filename) {
-    sStdoutLogger = spdlog::stdout_color_mt("stdout_logger");
-    sStderrLogger = spdlog::stderr_color_mt("stderr_logger");
+    sStdoutLogger = makeDedupedLogger<sink::stdout_color_sink_mt>("stdout_logger");
+    sStderrLogger = makeDedupedLogger<sink::stderr_color_sink_mt>("stderr_logger");
 
     try {
-        sFileLogger = spdlog::basic_logger_mt("file_logger", filename.data());
+        sFileLogger = makeDedupedLogger<sink::basic_file_sink_mt>("file_logger", std::string { filename });
     } catch (const spdlog::spdlog_ex &ex) {
         std::cerr << "[Logging::Open] Failed to open logfile at path \"" << filename << "\"!" << std::endl;
         std::cerr << "[Logging::Open] " << ex.what() << std::endl;
