106e38aea94f4416c26496cdac33418b80b1fc4e
Angelo <48489278+conhlee@users.noreply.github.com>
2026-01-14 22:48:09 +0100

    MPL2 ftw

--- src/compression/Yaz0/Window.hpp
+++ src/compression/Yaz0/Window.hpp
@@ -3,7 +3,8 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-    Ported to C++ by conhlee from original Rust source (18-07-2025)
+    Derived from the Orthrus project (https://github.com/NWPlayer123/Orthrus),
+    ported to C++ by conhlee, 2025
 */
 
 #ifndef YAZ0_WINDOW_HPP
